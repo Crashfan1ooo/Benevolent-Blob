99632ebefe2c9ba312166f38907e949ff1429ebe
Crashfan1ooo <saraviagio1@yahoo.com>
2021-11-16 19:28:09 -0500

    Fixed coin exploit

--- The Benevolent Blob/Assets/Scripts/CoinScoreKeeper.cs
+++ The Benevolent Blob/Assets/Scripts/CoinScoreKeeper.cs
@@ -9,34 +9,53 @@ public class CoinScoreKeeper : MonoBehaviour
 
     public GameObject collectibleText;
 
-    public  static int collectibleNumber;
+    public static int collectibleNumber;
 
     public static int lastCoins;
+
+    private bool coinProcess2 = false;
+    private bool coinProcess3 = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        Scene currentScene = SceneManager.GetActiveScene();
 
-        string sceneName = currentScene.name;
-        
-        if (sceneName == "Level 2")
-        {
-            lastCoins = collectibleNumber;
-        }
-
-        else if (sceneName == "Level 3")
-        {
-            lastCoins = collectibleNumber;
-        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("Level 2"))
+        {
+            if (coinProcess2 == false)
+            {
+                SceneChecker();
+                coinProcess2 = true;
+            }
+        }
+
+        if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("Level 3"))
+        {
+            if (coinProcess3 == false)
+            {
+                SceneChecker();
+                coinProcess3 = true;
+            }
+        }
 
         //Displays the current number of coins.
         collectibleText.GetComponent<Text>().text = "Coins: " + collectibleNumber;
 
     }
+
+    void SceneChecker()
+    {
+        lastCoins = collectibleNumber;
+
+    }
+
 }
+
+
+    
+
--- The Benevolent Blob/Assets/Scripts/KillPlayerL3.cs
+++ The Benevolent Blob/Assets/Scripts/KillPlayerL3.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class KillPlayerL3 : MonoBehaviour
-{
+{ 
+    public GameObject CoinChecker2;
+    public CoinScoreKeeper coinScript2;
     public int Respawn;
     // Start is called before the first frame update
     void Start()
     {
-        
+        CoinChecker2 = GameObject.Find("UI Hub");
+        coinScript2 = CoinChecker2.GetComponent<CoinScoreKeeper>();
     }
 
     // Update is called once per frame
@@ -22,6 +25,7 @@ public class KillPlayerL3 : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
+            CoinScoreKeeper.collectibleNumber = CoinScoreKeeper.lastCoins;
             SceneManager.LoadScene(Respawn);
         }
     }
