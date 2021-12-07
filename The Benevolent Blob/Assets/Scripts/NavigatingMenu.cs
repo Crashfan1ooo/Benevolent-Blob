4651c2b5e3a802ae36b53cc4ccd93665a85b84d5
Crashfan1ooo <saraviagio1@yahoo.com>
2021-12-07 03:36:27 -0500

    Messing with menu navigation

---
+++ The Benevolent Blob/Assets/Scripts/NavigatingMenu.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class NavigatingMenu : MonoBehaviour
+{
+    /*
+    public GameObject menuFirstButton;
+    public GameObject keybindingMenu;
+    public GameObject colorblindMenu;
+    public GameObject storyMenu;
+    public GameObject mainMenu;
+    public GameObject keybindingFirstButton;
+    public GameObject keybindingClosedButton;
+    */
+    // Start is called before the first frame update
+    void Start()
+    {
+        //clears selected object
+        //EventSystem.current.SetSelectedGameObject(null);
+
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        if(keybindingMenu.activeSelf == true)
+        {
+            EventSystem.current.SetSelectedGameObject(keybindingFirstButton);
+        }
+
+        if(mainMenu.activeSelf == true)
+        {
+            EventSystem.current.SetSelectedGameObject(menuFirstButton);
+        }
+        */
+    }
+}
